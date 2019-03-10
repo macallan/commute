0b95ae10dbaad311913855d553c9d20c5a3a8e15
Macallan Brown <macallan.brown@looker.com>
2019-03-09 20:21:10 -0800

    Sign up name validation

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import CommuteNavBar from './Components/CommuteNavBar'
-import { Button, Form, FormGroup, Label, Input} from 'reactstrap'
+import { Button, Form, FormFeedback, FormGroup, Label, Input} from 'reactstrap'
 import { Redirect } from 'react-router-dom'
 
 class SignUp extends Component {
   state = {
     toHome: false,
+    nameTaken: false,
     name: '',
     distance: ''
   }
 
   handleSubmit() {
-    
     console.log("Submit button pressed")
   }
 
@@ -43,10 +43,13 @@ class SignUp extends Component {
         this.setState({ 
           toHome: true
         })
+      } else if (response.status === 422) {
+        this.setState({ 
+          nameTaken: true
+        })
       }
     }).catch(function(err) {
       console.log("Error")
-      return
     })
   }
 
@@ -60,10 +63,11 @@ class SignUp extends Component {
         <Form onSubmit={ (e) => this.submitForm(e) }>
           <FormGroup>
             <Label for="name">Name</Label>
-            <Input type="name" name="name" id="name" placeholder="Bailey Biker" value={ this.state.name } onChange={ (e) => this.setName(e) }></Input>
+            <Input type="name" name="name" id="name" placeholder="Bailey Biker" value={ this.state.name } onChange={ (e) => this.setName(e) } invalid={this.state.nameTaken}></Input>
+            <FormFeedback>That name is already taken, please use a different one.</FormFeedback>
           </FormGroup>
           <FormGroup>
-            <Label for="distance">Distance in Miles</Label>
+            <Label for="distance">Commute Distance in Miles</Label>
             <Input type="decimal" id="distance" placeholder="1.2" value={ this.state.distance } onChange={ (e) => this.setDistance(e) }></Input>
           </FormGroup>
           <Button>Submit</Button>
